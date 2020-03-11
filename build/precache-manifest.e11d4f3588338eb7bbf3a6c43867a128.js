fc8a10a8c9b9e59fdd9932ccd765788c48f77eb3
Pietari Nurmi <pietari@nyxo.fi>
2020-03-11 11:03:53 +0200

    corrected index.html problem by adding homepage to package.json

---
+++ build/precache-manifest.e11d4f3588338eb7bbf3a6c43867a128.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bad599392e70f4c0901ff80cbbbb6c96",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e4b2ec2e5717898f2d5e",
+    "url": "./static/css/main.e75e77db.chunk.css"
+  },
+  {
+    "revision": "17efbe1fc256a33ef33e",
+    "url": "./static/js/2.336627b8.chunk.js"
+  },
+  {
+    "revision": "c27e167d6e2cc1d9eddf7fde2be56a6c",
+    "url": "./static/js/2.336627b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4b2ec2e5717898f2d5e",
+    "url": "./static/js/main.179c23e3.chunk.js"
+  },
+  {
+    "revision": "f043906b1372d731247a",
+    "url": "./static/js/runtime-main.4df6c47c.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.25855a34269aa56ad6e10b09a7f5b1bb.js"
+  "./precache-manifest.e11d4f3588338eb7bbf3a6c43867a128.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
